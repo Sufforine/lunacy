e6171f029841db0841bd8ff3e18667ad3a7f2283
Sufforine <dav89374626363@gmail.com>
2026-04-01 19:09:00 +0300

    hub
    пуш хаба на мейн

--- Scripts/MainMenu/main_menu.gd
+++ Scripts/MainMenu/main_menu.gd
@@ -3,3 +3,8 @@ extends Control
 
 func _on_debug_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mission.tscn") # Replace with function body.
+
+
+func _on_start_pressed() -> void:
+		get_tree().change_scene_to_file("res://scenes/hub.tscn") # Replace with function body.
+ # Replace with function body.
