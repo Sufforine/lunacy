2586783f7aaf6355385c76ec59888310e1d80fcb
Hron0 <sasha1254370@gmail.com>
2026-04-20 18:01:03 +0300

    В пизду

--- Scripts/Inventory/UI/Slot/SlotUI.gd
+++ Scripts/Inventory/UI/Slot/SlotUI.gd
@@ -4,7 +4,6 @@ extends PanelContainer
 @onready var quantity_label = $Quantity
 
 var slot_index: int
-var grab_amount: int = 0
 
 static var active_drag_data = null
 
@@ -23,73 +22,70 @@ func display(slot_data):
 
 func create_preview():
 	var preview = TextureRect.new()
-	preview.z_index = 50
+	preview.z_index = 160
 	preview.texture = icon_rect.texture
 	preview.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	preview.custom_minimum_size = Vector2(50, 50)
 	
 	var previewLabel = Label.new()
-	previewLabel.text = str(max(1, grab_amount))
+	previewLabel.text = str(1)
 	previewLabel.name = "CountLabel"
 	previewLabel.set_anchors_and_offsets_preset(Control.PRESET_BOTTOM_RIGHT, Control.PRESET_MODE_MINSIZE)
 	preview.add_child(previewLabel)
 	
 	return preview
 
-func create_drag_data(is_split: bool):
-	var inventory_data = GlobalRefs.player_inventory_root.inventory_data
-	var slot_data = inventory_data.slots[slot_index]
-	
-	var drag_data = {
+func create_drag_data_for_split(item: Item):
+	return {
 		"origin_index": slot_index,
-		"item": slot_data.item,
-		"amount": 0,
-		"is_split": is_split
+		"item": item,
+		"amount": 1,
+		"is_split": true
 	}
-	
-	if is_split:
-		drag_data.amount = grab_amount
-		grab_amount = 0 # Сброс после захвата
-	else:
-		drag_data.amount = slot_data.amount
-		inventory_data.remove_at(slot_index) # Забираем всё
-		
-	return drag_data
 
 func _get_drag_data(_at_position):
-	var slot_data = GlobalRefs.player_inventory_root.inventory_data.slots[slot_index]
+	var inventory_data = GlobalRefs.player_inventory_root.inventory_data
+	var slot_data = inventory_data.slots[slot_index]
 	if slot_data == null: return null
 	
-	var data = create_drag_data(false)
+	if active_drag_data != null: return null
 	
-	if data == null: return null
+	active_drag_data = {
+		"origin_index": slot_index,
+		"item": slot_data.item,
+		"amount": slot_data.amount,
+		"is_split": false
+	}
+	
 	var dragPreview = create_preview()
-	
 	set_drag_preview(dragPreview)
-	active_drag_data = data
-	return data
-
+	
+	inventory_data.remove_at(slot_index)
+	return active_drag_data
 
 func _can_drop_data(_at_position, data):
 	return data is Dictionary
 
-
 func _drop_data(_at_position, data):
 	var inventory = GlobalRefs.player_inventory_root.inventory_data
-	
 #	index: int, item_to_add: Item, amount: int
 	inventory.place_at(slot_index, data.item, data.amount)
 
-
+# В пизду
 func _gui_input(event):
+	var inventory_data = GlobalRefs.player_inventory_root.inventory_data
+	var slot_data = inventory_data.slots[slot_index]
+	
 	if event is InputEventMouseButton and event.pressed:
-		var inventory_ui = GlobalRefs.player_inventory_root
-		
-		# Если нажали ПКМ и зажат Shift — отделяем 1 предмет
 		if event.button_index == MOUSE_BUTTON_RIGHT and Input.is_key_pressed(KEY_SHIFT):
-			inventory_ui.inventory_data.split_stack(slot_index, 1)
-#			force_drag() тут
-
+			if slot_data and slot_data.amount > 1:
+				if active_drag_data == null:
+					slot_data.amount -= 1
+					active_drag_data = create_drag_data_for_split(slot_data.item)
+					var preview_node = create_preview()
+					
+					inventory_data.update_slots.emit()
+					force_drag(active_drag_data, preview_node)
 
 func _notification(what):
 	if what == NOTIFICATION_DRAG_END:
