68246c04886a0e4cee1b4a6fa32a61ed72bbbe98
Hron0 <sasha1254370@gmail.com>
2026-07-03 14:22:29 +0300

    clean

--- screens/main-menu/ui/main_menu.gd
+++ screens/main-menu/ui/main_menu.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _on_debug_pressed() -> void:
-	get_tree().change_scene_to_file("res://screens/mission/ui/mission.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://screens/mission/ui/mission.tscn")
 
 
 func _on_start_pressed() -> void:
-		get_tree().change_scene_to_file("res://screens/character-select/ui/character_select.tscn") # Replace with function body.
- # Replace with function body.
+		get_tree().change_scene_to_file("res://screens/character-select/ui/character_select.tscn")
+
 
 
 func _on_host_game_pressed() -> void:
